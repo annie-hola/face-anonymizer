cfddd5086877c1639b87f1a9d1a1e39f5384e77a
Ha Kieu Anh <hakieuanh@Annies-Mac.local>
2024-01-25 17:32:28 +0700

    default set camera

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
         output_video.release()
 
     elif args.mode in ['webcam']:
-        cap = cv2.VideoCapture(2)
+        cap = cv2.VideoCapture(0) #default set 0
 
         ret, frame = cap.read()
         while ret:
